09f6d0ddbbaf9a3d980b6310e4cf88e26266a3ce
Chris Mosier <mosierc@oregonstate.edu>
2026-02-12 17:02:22 -0800

    Iteration on project. added data files to save venues, teams, games and game data.

--- server.py
+++ server.py
@@ -1,26 +1,98 @@
 from flask import Flask, request, render_template
 import json
+from datetime import datetime
 
 
 app = Flask(__name__)
 
+# function to read in the data for all teams
 def read_in_teams():
     with open('data/teams.json') as f:
         teams = json.load(f)
         return teams
 
+#function to write the updated data for all teams
 def write_to_teams_data(new_team_data):
     with open('data/teams.json', 'w') as file:
         json.dump(new_team_data, file)
 
+# function to read in the data for all venues
+def read_in_venues():
+    with open('data/venues.json') as f:
+        venues = json.load(f)
+        return venues
+
+#function to write the updated data for all venues
+def write_to_venues_data(new_venue_data):
+    with open('data/venues.json', 'w') as file:
+        json.dump(new_venue_data, file)
+
+# function to read in the data for all games
+def read_in_games():
+    with open('data/games.json') as f:
+        games = json.load(f)
+        return games
+
+#function to write the updated data for all games
+def write_to_games(new_game_data):
+    with open('data/games.json', 'w') as file:
+        json.dump(new_game_data, file)
+
+# function to read in the data for all game data
+def read_in_game_data():
+    with open('data/game_results.json') as f:
+        game_results = json.load(f)
+        return game_results
+
+#function to write the updated data for all game data
+def write_to_game_data(new_game_results):
+    with open('data/game_results.json', 'w') as file:
+        json.dump(new_game_results, file)
+
+# the home page includes a list of all upcoming games
 @app.route('/')
 def home_page():
-    return render_template('index.html', team_name="West Ham")
+    # list to hold all upcoming games
+    upcoming = []
 
-# list of all upcoming games
+    # loop through all games and only add games where the status is "scheduled"
+    for game in games:
+        if game['status'] == 'scheduled':
+            upcoming.append(game)
+
+    # list to hold the game info (teams who are playing, venue, game date)
+    upcoming_game_data = []
+
+    # loop through upcoming[] list and merge required data from teams and venues
+    for game in upcoming:
+        # switch datetime string to datetime object
+        utc_time = datetime.fromisoformat(game['datetime'])
+
+        # dictionary of individual game data
+        individual_game = {
+            'id': game['id'],
+            'datetime': utc_time.strftime('%A, %B %d, %Y at %I:%M %p %Z'),
+            'home_team': teams_dict[game['home_team_id']]['name'],
+            'away_team': teams_dict[game['away_team_id']]['name'],
+            'venue': venues_dict[game['venue_id']]['name'],
+            'city': venues_dict[game['venue_id']]['city'],
+            'country': venues_dict[game['venue_id']]['country']
+        }
+
+        # add individual game back into list of upcoming games
+        upcoming_game_data.append(individual_game)
+
+    return render_template('index.html', games=upcoming_game_data)
+
+# the games page includes a list of all games (past and present) for this season
 @app.route('/games')
 def list_games():
     return render_template('list_games.html')
+
+# list all teams
+@app.route('/teams')
+def list_teams():
+    return render_template('list_teams.html', ListofTeams=teams)
 
 # list info for a specific game
 @app.route('/game_detail')
@@ -30,7 +102,7 @@ def game_detail():
 # admin: add a new team form
 @app.route('/admin/add_team')
 def admin_add_team_form():
-    return render_template('admin/admin_add_team.html')
+    return render_template('admin/admin_add_team.html', ListofTeams=teams)
 
 # admin: add team to database
 @app.post('/admin/create_new_team')
@@ -46,10 +118,6 @@ def create_new_team():
     write_to_teams_data(teams)
     return f'sucessfully added {team_name} {team_countryID} {id}'
 
-@app.get('/teams')
-def list_teams():
-    return teams
-
 @app.get('/teams/<id>')
 def read_single_team(id):
     for team in teams:
@@ -59,6 +127,28 @@ def read_single_team(id):
     return "That team doesn't exist"
 
 if __name__ == '__main__':
-    #load data
+    #load teams data from file
     teams = read_in_teams()
+
+    #load games from file
+    games = read_in_games()
+
+    #load game data from file
+    game_data = read_in_game_data()
+
+    #load venue data from file
+    venues = read_in_venues()
+
+    #create a dictionary of teams
+    teams_dict = {}
+    for team in teams:
+        team_id = team['id']
+        teams_dict[team_id] = team
+
+    #create a dictionary of venues
+    venues_dict = {}
+    for venue in venues:
+        venue_id = venue['id']
+        venues_dict[venue_id] = venue
+
     app.run(debug=True)
